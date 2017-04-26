b03e16bfcfffadb30f2ce57a2de1baa2e14320c0
jgarriuni <jgarri04@ucm.es>
2017-04-26 10:04:03 +0200

    Metido el proyecto en github

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function () {});
+$(document).ready(function () {$('.slider').slider(); });
+$(".button-collapse").sideNav();
+ $(document).ready(function(){
+    $('ul.tabs').tabs();
+  });
